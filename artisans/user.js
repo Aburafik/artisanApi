e8e2fef272bf1e6af7b292de16dc8efcb7399873
Aburafik <rfkabubakari@gmail.com>
2021-06-18 14:15:52 -0700

    first commit

---
+++ artisans/user.js
@@ -0,0 +1,15 @@
+const cap = [
+    { id: 1, name: 'Alan ', age: 21, city: 'New York' },
+    { id: 2, name: 'Steve', age: 106, city: 'Chicago' },
+    { id: 3, name: 'Tom ', age: 47, city: 'Detroit' },
+    { id: 4, name: 'Ryan', age: 16, city: 'New York' },
+    { id: 5, name: 'Jack ', age: 31, city: 'New York' },
+    { id: 6, name: 'Clark', age: 34, city: 'Metropolis' },
+    { id: 7, name: 'Bruce Wayne', age: 21, city: 'Gotham' },
+    { id: 8, name: 'Tim Drake', age: 21, city: 'Gotham' },
+    { id: 9, name: 'Jimmy Olsen', age: 21, city: 'Metropolis' },
+    { id: 10, name: 'Ryan Burns', age: 21, city: 'New York' },
+];
+
+
+module.exports = cap;
---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+
+const express = require('express');
+
+const myData = require('./data');
+const mydata = require('./artisans/user');
+const app = express();
+
+
+// Initialize App
+
+
+// Assign route
+app.use('/', (req, res, next) => {
+    const filters = req.query;
+    const filteredUsers = myData.filter(user => {
+        let isValid = true;
+        for (key in filters) {
+            console.log(key, user[key], filters[key]);
+            isValid = isValid && user[key] == filters[key];
+        }
+        return isValid;
+    });
+    res.send(filteredUsers);
+});
+
+app.use('/', (req, res, next) => {
+    const filters = req.query;
+    const filteredUsers = cap.filter(user => {
+        let isValid = true;
+        for (key in filters) {
+            console.log(key, user[key], filters[key]);
+            isValid = isValid && user[key] == filters[key];
+        }
+        return isValid;
+    });
+    res.send(filteredUsers);
+});
+
+// Start server on PORT 5000
+app.listen(5000, () => {
+    console.log('Server started!');
+    // console.log()
+});
