0085e7752377fb55c1fdb83f15f2ac22f2ae735b
Aburafik <rfkabubakari@gmail.com>
2021-06-18 16:40:02 -0700

    remove id

--- user.js
+++ user.js
@@ -1,14 +1,14 @@
 const cap = [
-    { id: 1, name: 'Rafik ', age: 21, city: 'New York' },
-    { id: 2, name: 'Steve', age: 106, city: 'Chicago' },
-    { id: 3, name: 'Tom ', age: 47, city: 'Detroit' },
-    { id: 4, name: 'Ryan', age: 16, city: 'New York' },
-    { id: 5, name: 'Jack ', age: 31, city: 'New York' },
-    { id: 6, name: 'Clark', age: 34, city: 'Metropolis' },
-    { id: 7, name: 'Bruce Wayne', age: 21, city: 'Gotham' },
-    { id: 8, name: 'Tim Drake', age: 21, city: 'Gotham' },
-    { id: 9, name: 'Jimmy Olsen', age: 21, city: 'Metropolis' },
-    { id: 10, name: 'Ryan Burns', age: 21, city: 'New York' },
+    { name: 'Rafik ', age: 21, city: 'New York', contact: "tel:0551143980" },
+    { name: 'Steve', age: 106, city: 'Chicago', contact: "tel:0551143980" },
+    { name: 'Tom ', age: 47, city: 'Detroit', contact: "tel:0551143980" },
+    { name: 'Ryan', age: 16, city: 'New York', contact: "tel:0551143980" },
+    { name: 'Jack ', age: 31, city: 'New York', contact: "tel:0551143980" },
+    { name: 'Clark', age: 34, city: 'Metropolis', contact: "tel:0551143980" },
+    { name: 'Bruce Wayne', age: 21, city: 'Gotham', contact: "tel:0551143980" },
+    { name: 'Tim Drake', age: 21, city: 'Gotham', contact: "tel:0551143980" },
+    { name: 'Jimmy Olsen', age: 21, city: 'Metropolis', contact: "tel:0551143980" },
+    { name: 'Ryan Burns', age: 21, city: 'New York', contact: "tel:0551143980" },
 ];
 
 
