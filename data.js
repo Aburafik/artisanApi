696a1f98a68007a258aea8f0cc7cbe28c95b8e2c
Aburafik <rfkabubakari@gmail.com>
2021-06-18 17:19:40 -0700

    still on it

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 const data = [
-    { name: 'Adam', age: 21, city: 'New York', contact: "tel:0551143980" },
-    { name: 'Steve Rogers', age: 106, city: 'Chicago', contact: "tel:0551143980" },
-    { name: 'Tom Hanks', age: 47, city: 'Detroit', contact: "tel:0551143980" },
-    { name: 'Ryan Burns', age: 16, city: 'New York', contact: "tel:0551143980" },
-    { name: 'Jack Ryan', age: 31, city: 'New York', contact: "tel:0551143980" },
-    { name: 'Clark Kent', age: 34, city: 'Metropolis', contact: "tel:0551143980" },
-    { name: 'Bruce Wayne', age: 21, city: 'Gotham', contact: "tel:0551143980" },
-    { name: 'Tim Drake', age: 21, city: 'Gotham', contact: "tel:0551143980" },
-    { name: 'Jimmy Olsen', age: 21, city: 'Metropolis', contact: "tel:0551143980" },
-    { name: 'Ryan Burns', age: 21, city: 'New York', contact: "tel:0551143980" },
+    { name: 'Adam', age: 21, city: 'New York', contact: ":0551143980" },
+    { name: 'Steve Rogers', age: 106, city: 'Chicago', contact: ":0551143980" },
+    { name: 'Tom Hanks', age: 47, city: 'Detroit', contact: ":0551143980" },
+    { name: 'Ryan Burns', age: 16, city: 'New York', contact: ":0551143980" },
+    { name: 'Jack Ryan', age: 31, city: 'New York', contact: ":0551143980" },
+    { name: 'Clark Kent', age: 34, city: 'Metropolis', contact: ":0551143980" },
+    { name: 'Bruce Wayne', age: 21, city: 'Gotham', contact: ":0551143980" },
+    { name: 'Tim Drake', age: 21, city: 'Gotham', contact: ":0551143980" },
+    { name: 'Jimmy Olsen', age: 21, city: 'Metropolis', contact: ":0551143980" },
+    { name: 'Ryan Burns', age: 21, city: 'New York', contact: ":0551143980" },
 ];
 
 
--- user.js
+++ user.js
@@ -1,14 +1,14 @@
 const cap = [
-    { name: 'Rafik ', age: 21, city: 'New York', contact: "tel:0551143980" },
-    { name: 'Steve', age: 106, city: 'Chicago', contact: "tel:0551143980" },
-    { name: 'Tom ', age: 47, city: 'Detroit', contact: "tel:0551143980" },
-    { name: 'Ryan', age: 16, city: 'New York', contact: "tel:0551143980" },
-    { name: 'Jack ', age: 31, city: 'New York', contact: "tel:0551143980" },
-    { name: 'Clark', age: 34, city: 'Metropolis', contact: "tel:0551143980" },
-    { name: 'Bruce Wayne', age: 21, city: 'Gotham', contact: "tel:0551143980" },
-    { name: 'Tim Drake', age: 21, city: 'Gotham', contact: "tel:0551143980" },
-    { name: 'Jimmy Olsen', age: 21, city: 'Metropolis', contact: "tel:0551143980" },
-    { name: 'Ryan Burns', age: 21, city: 'New York', contact: "tel:0551143980" },
+    { name: 'Rafik ', age: 21, city: 'New York', contact: "0551143980" },
+    { name: 'Steve', age: 106, city: 'Chicago', contact: "0551143980" },
+    { name: 'Tom ', age: 47, city: 'Detroit', contact: "0551143980" },
+    { name: 'Ryan', age: 16, city: 'New York', contact: "0551143980" },
+    { name: 'Jack ', age: 31, city: 'New York', contact: "0551143980" },
+    { name: 'Clark', age: 34, city: 'Metropolis', contact: "0551143980" },
+    { name: 'Bruce Wayne', age: 21, city: 'Gotham', contact: "0551143980" },
+    { name: 'Tim Drake', age: 21, city: 'Gotham', contact: "0551143980" },
+    { name: 'Jimmy Olsen', age: 21, city: 'Metropolis', contact: "0551143980" },
+    { name: 'Ryan Burns', age: 21, city: 'New York', contact: "0551143980" },
 ];
 
 
